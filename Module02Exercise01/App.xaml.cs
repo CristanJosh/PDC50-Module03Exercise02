4b52032e208c9d1377cc53be69a4347ce28bb7fc
CristanJosh <cristanjosh03@gmail.com>
2024-09-11 10:19:20 +0800

    Add project files.

---
+++ Module02Exercise01/App.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using Module02Exercise01.View;
+using Microsoft.Maui.Networking;
+using Module02Exercise01.Services;
+
+namespace Module02Exercise01
+{
+    public partial class App : Application
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public App(IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            _serviceProvider = serviceProvider;
+
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CheckNetworkConnectivity();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            Debug.WriteLine("The app is in sleep mode.");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Debug.WriteLine("The app has resumed.");
+            CheckNetworkConnectivity();
+        }
+
+        private async void CheckNetworkConnectivity()
+        {
+            var current = Connectivity.Current;
+
+            if (current.NetworkAccess == NetworkAccess.Internet)
+            {
+                //Attempt I-ping yung website if its accessible ig? whahaha
+                try
+                {
+                    using var httpClient = new HttpClient();
+                    var result = await httpClient.GetAsync("https://google.com");
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        // Navigate to LoginPage if connected
+                        //MainPage = new NavigationPage(new LoginPage());
+                        MainPage = _serviceProvider.GetRequiredService<LoginPage>();
+                    }
+                    else
+                    {
+                        // Navigate to OfflinePage if ping fails
+                        //MainPage = new OfflinePage();
+                        MainPage = _serviceProvider.GetRequiredService<OfflinePage>();
+                    }
+                }
+                catch
+                {
+
+                    MainPage = _serviceProvider.GetRequiredService<OfflinePage>();
+
+                }
+            }
+            else
+            {
+
+                MainPage = _serviceProvider.GetRequiredService<OfflinePage>();
+
+            }
+        }
+    }
+}
---
+++ Module02Exercise01/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module02Exercise01.Model;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace Module02Exercise01.ViewModel
+{
+    public class EmployeeViewModel : INotifyPropertyChanged
+    {
+        private Employee _manager;
+
+        public EmployeeViewModel()
+        {
+            // Initialize a sample employee manager model
+            _manager = new Employee
+            {
+                FirstName = "Cristan Josh",
+                LastName = "Nuguid",
+                Position = "Manager",
+                Department = "IT",
+                IsActive = true
+            };
+
+            // UI Thread Management
+            LoadEmployeeDataCommand = new Command(async () => await LoadEmployeeDataAsync());
+
+            // Initial list of employees
+            Employees = new ObservableCollection<Employee>
+            {
+                new Employee { FirstName = "Lorenzo", LastName = "Sangalang", Position = "Developer", Department = "IT", IsActive = true },
+                new Employee { FirstName = "Richard", LastName = "Sy", Position = "Developer", Department = "IT", IsActive = true },
+                new Employee { FirstName = "Mark", LastName = "Soberano", Position = "Analyst", Department = "Finance", IsActive = true },
+                new Employee { FirstName = "Derick", LastName = "Pangilinan", Position = "Support", Department = "Customer Service", IsActive = false }
+            };
+        }
+
+        public ObservableCollection<Employee> Employees { get; set; }
+
+        private string _managerName;
+        public string ManagerName
+        {
+            get => _managerName;
+            set
+            {
+                if (_managerName != value)
+                {
+                    _managerName = value;
+                    OnPropertyChanged(nameof(ManagerName));
+                }
+            }
+        }
+
+        public ICommand LoadEmployeeDataCommand { get; }
+
+        private async Task LoadEmployeeDataAsync()
+        {
+            await Task.Delay(1000);
+            ManagerName = $"{_manager.FullName} ({_manager.Position})";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
